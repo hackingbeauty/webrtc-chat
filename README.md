00f41c502ee7c36c4e73055604440fb0b2f43a64
Mark Muskardin <mark.muskardin@gmail.com>
2013-10-16 10:43:45 -0700

    added facebook authentication

--- README.md
+++ README.md
@@ -5,3 +5,31 @@
 * start redis command: 
 
 	redis-server
+
+* start mongodb command:
+
+	mongod
+
+* start mongod console:
+
+	mongo
+
+* create a mongo collection:
+
+	db.createCollection('collection name')
+
+* show all mongodb collections:
+
+	db.getCollectionNames()
+
+
+
+
+
+NOTES TO SELF ----
+
+* when pushing to production, you will have to specify the following:
+
+	a mongodb url on prod
+	a redis db url on prod
+	twitter oauth key/secret for prod
--- server.js
+++ server.js
@@ -12,7 +12,12 @@ var app = express();
 var port = '3900'; //process.env.PORT for production - use 'process.env.PORT || "3800"'
 var io = require('socket.io').listen(app.listen(port));
 var passport = require('passport'),
-	TwitterStrategy = require('passport-twitter').Strategy;
+	FacebookStrategy = require('passport-facebook').Strategy;
+var store = new express.session.MemoryStore;
+var MongoStore = require('connect-mongo')(express);
+var mongoose = require ("mongoose");
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/webrtc';
+
 
 // var redis = require('redis');
 var redis = require('redis-url').connect(process.env.REDISTOGO_URL || '6379');
@@ -21,6 +26,13 @@ redis.set('onlineUsers', onLineUsers);
 
 
 // all environments
+app.use(express.cookieParser());
+app.use(express.session({
+  store: new MongoStore({
+    url: 'mongodb://localhost/webrtc'
+  }),
+  secret: '1234567890QWERTY'
+}));
 app.set('port', port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -31,6 +43,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -43,30 +56,43 @@ app.get('/room/new', function(req, res){
 	res.send("respond with a resource");
 });
 
-// app.post('/login', passport.authenticate('local', { successRedirect: '/room/new',
-//                                                     failureRedirect: '/login' }));
-
-passport.use(new TwitterStrategy({
-    consumerKey: TWITTER_CONSUMER_KEY,
-    consumerSecret: TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://www.example.com/auth/twitter/callback"
+passport.use(new FacebookStrategy({
+    clientID: '218363748287603',
+    clientSecret: '58a7e992463b6a17b73ccee8b0c0dfee',
+    callbackURL: "/auth/facebook/callback"
   },
   function(token, tokenSecret, profile, done) {
-    User.findOrCreate(..., function(err, user) {
+  	console.log('token: ',token);
+  	console.log('tokenSecret: ',tokenSecret);
+  	console.log('profile: ', profile);
+    User.findOrCreate(function(err, user) {
       if (err) { return done(err); }
       done(null, user);
     });
   }
 ));
 
-app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/facebook', passport.authenticate('facebook'));
 
 // Twitter will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
-app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { successRedirect: '/',
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
                                      failureRedirect: '/login' }));
+
+
+
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 
