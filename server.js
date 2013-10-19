6204ba8168a07745d3256f9f60fb9eb2cfe43331
Mark Muskardin <mark.muskardin@gmail.com>
2013-10-19 14:58:42 -0700

    reorganizing and cleaning up server.js

--- server.js
+++ server.js
@@ -1,76 +1,63 @@
+/*jslint         node    : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+/*global */
 
-/**
- * Module dependencies.
- */
+// ------------ BEGIN MODULE SCOPE VARIABLES --------------
 
-var express = require('express');
-var routes = require('./routes');
-// require('./lib/socket')(io,onLineUsers);
-var http = require('http');
-var path = require('path');
-var app = express();
-var port = '3900'; //process.env.PORT for production - use 'process.env.PORT || "3800"'
-var io = require('socket.io').listen(app.listen(port));
-var passport = require('passport'),
-	FacebookStrategy = require('passport-facebook').Strategy;
-var store = new express.session.MemoryStore;
-var MongoStore = require('connect-mongo')(express);
-var mongoose = require ("mongoose");
-var findOrCreate = require('mongoose-findorcreate')
-var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/webrtc';
-var jade = require('jade');
+'use strict';
+var 
+    express           = require('express'),
+    routes            = require('./routes'),
+    http              = require('http'),
+    path              = require('path'),
+    app               = express(),
+    port              = '3900', //process.env.PORT for production - use 'process.env.PORT || "3800"'
+    io                = require('socket.io').listen(app.listen(port)),
+    passport          = require('passport'),
+    FacebookStrategy  = require('passport-facebook').Strategy,
+    store             = new express.session.MemoryStore,
+    MongoStore        = require('connect-mongo')(express),
+    mongoose          = require ("mongoose"),
+    findOrCreate      = require('mongoose-findorcreate'),
+    uristring         = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/webrtc',
+    jade              = require('jade');
 
+// ------------- END MODULE SCOPE VARIABLES ---------------
 
-// var redis = require('redis');
-var redis = require('redis-url').connect(process.env.REDISTOGO_URL || '6379');
-var onLineUsers = [];
-redis.set('onlineUsers', onLineUsers);
+// ------------- BEGIN SERVER CONFIGURATION ---------------
 
-
-
-
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
-  if (err) { 
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-  console.log ('Succeeded connected to: ' + uristring);
-  }
+app.configure( function() {
+    app.use(express.bodyParser()); // decodes forms
+    app.use(express.methodOverride()); // used for creating RESTful services
+    app.use(passport.initialize());
+    app.set('port', port);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(app.router);
+    app.use(require('stylus').middleware(__dirname + '/public'));
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
-// Mongoose schema
-var UserSchema = new mongoose.Schema({
-	firstName: { type: String },
-	lastName: { type: String },
-	age: { type: Number, min: 0},
-	facebookID: { type: Number },
-	email: {type: String }
+app.configure( 'development', function() {
+    app.use( express.logger() );
+    app.use( express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
 });
 
-UserSchema.plugin(findOrCreate); // Gives you the ability to do User.findOrCreate
-var User = mongoose.model('User', UserSchema);
-
-// all environments
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.session({
-  store: new MongoStore({
-    url: 'mongodb://localhost/webrtc'
-  }),
-  secret: '1234567890QWERTY'
-}));
-app.use(passport.initialize());
-app.set('port', port);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
+app.configure( 'production', function() {
+    app.use( express.errorHandler() );
+});
 app.use(express.logger('dev'));
 
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 
 // development only
@@ -81,7 +68,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 app.get('/room/main_room', function(req, res){
-  res.render('main_room', { title: 'WebRTC App' });
+  res.render('main_room', { title: 'Video Debate' });
 
 });
 
@@ -134,3 +121,34 @@ app.get('/auth/facebook/callback',
 
 
 
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+// Mongoose schema
+var UserSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  age: { type: Number, min: 0},
+  facebookID: { type: Number },
+  email: {type: String }
+});
+
+UserSchema.plugin(findOrCreate); // Gives you the ability to do User.findOrCreate
+var User = mongoose.model('User', UserSchema);
+
+// all environments
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({
+  store: new MongoStore({
+    url: 'mongodb://localhost/webrtc'
+  }),
+  secret: '1234567890QWERTY'
+}));
