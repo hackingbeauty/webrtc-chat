758632ae22c460fecaf3d2d6625662bca5606395
Mark Muskardin <mark.muskardin@gmail.com>
2013-10-25 13:58:15 -0700

    added getUserMedia...video object or user is showing video stream

--- public/javascripts/lib/rtc_client.js
+++ public/javascripts/lib/rtc_client.js
@@ -1,15 +1,47 @@
-(function(global){
+/*jslint         node    : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+/*global */
 
+'use strict';
 
-	// global.RTC = window.unnamed.RTC = {
-	// 	init: init,
-	// 	join: join,
-	// 	caller: caller,
-	// 	receiver: receiver,
-	// 	leave: leave,
-	// 	sendMessage: sendMessage,
-	// 	onForceClosed: null,
-	// 	onMessage: null
-	// };
+WebrtcChat.RtcClient = (function(global){
+
+	var constraints = {
+		audio: true,
+		video: true
+	}
+
+	function addVideoStream(){
+		navigator.getMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);
+
+		navigator.getMedia(constraints, successfulStream, error);
+	}
+
+	function successfulStream(stream){
+		var video = document.getElementById('user-video');
+		video.src = window.URL.createObjectURL(stream);
+		video.play();
+	}
+
+	function error(){
+		console.log('you have an error!');
+	}
+
+	function init(){
+		addVideoStream();
+	}
+
+	return {
+		init: init
+	}
 
 })(window);
+
+WebrtcChat.RtcClient.init();
