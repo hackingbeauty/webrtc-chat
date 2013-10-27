a57256ac764657daa545abd9232f6048c5ef169a
Mark Muskardin <mark.muskardin@gmail.com>
2013-10-27 12:14:52 -0700

    added stubs for new specs

--- tests/routes.js
+++ tests/routes.js
@@ -4,14 +4,11 @@ var assert 	= require('assert'),
 
 describe('API', function(){
 	
-	var body;
-
 	describe('GET /api/rooms/list', function(){
 		
+		var body;
+
 		before(function(done){
-			// options =  {
-			// 	uri: 'blah'
-			// }
 			request('http://localhost:3900/api/rooms/list', function(err, resp, _body){
 				console.log('body is: ', JSON.parse(_body));
 				body = _body
@@ -19,8 +16,57 @@ describe('API', function(){
 			})
 		});
 		
-		it('should return a list of room', function(){
+		it('should return a list of rooms', function(){
 			assert.equal(true, true);
 		});
-	})
-})
+	
+	});
+
+	describe('POST /api/rooms/create', function(){
+
+		var body;
+
+		before(function(done){
+			request('http://localhost:3900/api/rooms/create', function(err, resp, _body){
+				console.log('body is: ', JSON.parse(_body));
+				body = _body
+				done();
+			})
+		});
+		
+		it('should create a room'); //tests without a callback are pending
+	});
+
+
+	describe('UPDATE /api/rooms/update', function(){
+
+		var body;
+
+		before(function(done){
+			request('http://localhost:3900/api/rooms/update', function(err, resp, _body){
+				console.log('body is: ', JSON.parse(_body));
+				body = _body
+				done();
+			})
+		});
+		
+		it('should update a room'); //tests without a callback are pending
+	});
+
+
+	describe('DELETE /api/rooms/delete', function(){
+
+		var body;
+
+		before(function(done){
+			request('http://localhost:3900/api/rooms/delete', function(err, resp, _body){
+				console.log('body is: ', JSON.parse(_body));
+				body = _body
+				done();
+			})
+		});
+		
+		it('should delete a room'); //tests without a callback are pending
+	});
+
+});
